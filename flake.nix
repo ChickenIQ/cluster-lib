5ee9ee369e400a989b82c2830638579616cc8881
ChickenIQ <emibenzar@gmail.com>
2026-08-30 02:21:55 +0300

    update inputs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,7 @@
 {
   inputs = {
-    nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
-
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
-    };
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
