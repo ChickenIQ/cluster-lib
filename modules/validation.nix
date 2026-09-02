f668ec88ab96657616a561a7a12f210ac978f412
ChickenIQ <emibenzar@gmail.com>
2026-09-02 20:33:55 +0300

    prevent conflict

--- modules/validation.nix
+++ modules/validation.nix
@@ -12,6 +12,16 @@
       cluster =
         v:
         let
+          modules = v.modules ++ lib.concatMap (c: c.modules) v.compartments;
+          applications = lib.concatMap (
+            module:
+            map (application: { name = application.metadata.name; }) (
+              builtins.filter (
+                resource: resource.kind or null == "Application" && resource.metadata.name or null != null
+              ) module.modules
+            )
+          ) modules;
+
           checks =
             lib.concatMap (c: [
               (unique "generator" (v.generators ++ c.generators))
@@ -20,11 +30,12 @@
             ]) v.compartments
             ++ [
               (unique "cluster file" ([ { name = "cluster-${v.name}"; } ] ++ v.modules))
-              (unique "module" (v.modules ++ lib.concatMap (c: c.modules) v.compartments))
+              (unique "application" (modules ++ applications))
               (unique "compartment" v.compartments)
               (unique "generator" v.generators)
               (unique "override" v.overrides)
               (unique "default" v.defaults)
+              (unique "module" modules)
             ];
         in
         assert lib.all lib.id checks;
